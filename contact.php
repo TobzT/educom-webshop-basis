189190b7531e78d35a0bda0e5be2375206f280bd
TobzT <tobias@conceptuals.nl>
2022-09-12 14:41:23 +0200

    functionaliteit #5

--- contact.php
+++ contact.php
@@ -77,6 +77,11 @@
         h1 {
             font-size: 40px;
         }
+
+        .error {
+            color: red;
+            display: inline;
+        }
     </style>
 </head>
 <body>
@@ -96,9 +101,9 @@
                         <label for="gender">Aanhef:</label>
 
                         <select name="gender" id="gender">
-                            <option value="Sir">Dhr</option>
-                            <option value="Madam">Mvr</option>
-                            <option value="Other">Anders</option>
+                            <option value="male">Dhr</option>
+                            <option value="female">Mvr</option>
+                            <option value="other">Anders</option>
                         </select><br><br>
 
                         <label for="name">Naam:</label>
@@ -113,10 +118,10 @@
                         <p>Communicatievoorkeur:</p>
                         
                         
-                        <input type="radio" id="vtlf" name="v" value="tlf">
+                        <input type="radio" id="vtlf" name="pref" value="tlf">
                         <label for="vtlf">Telefoon</label><br>
                         
-                        <input type="radio" id="vemail" name="v" value="email">
+                        <input type="radio" id="vemail" name="pref" value="email">
                         <label for="vemail">E-mail</label><br><br>
 
                         <textarea class="input" name="Text1" cols="40" rows="10"></textarea>
@@ -127,25 +132,129 @@
                 );
             };
 
+            $nameErr = $emailErr = $tlfErr = $prefErr = "";
+            $name = $email = $gender = $text = $pref = $tlf = "";
+            $valid = true;
+            $dropdown = $radio = $pronoun = "";
             if($_SERVER["REQUEST_METHOD"] == "POST") {
                 $gender = test_inputs($_POST["gender"]);
-                $name = test_inputs($_POST["name"]);
-                $email = test_inputs($_POST["email"]);
-                $tlf = test_inputs($_POST["tlf"]);
-                $v = test_inputs($_POST["v"]);
-                $text = test_inputs($_POST["Text1"]);
+                
+                if(empty($_POST["name"])) {
+                    $nameErr = "Je moet je naam invullen.";
+                    $valid = false;
+                } else {
+                    $name = test_inputs($_POST["name"]);
+                }
 
-                echo(
-                    "
-                        <p> gender: $gender </p>
-                        <p> name: $name </p>
-                        <p> email: $email </p>
-                        <p> tlf: $tlf </p>
-                        <p> voorkeur: $v </p>
-                        <p> message: $text </p>
-                    "
-                );
-            }
+                if(empty($_POST["email"])) {
+                    $emailErr = "Je moet je e-mail adres invullen.";
+                    $valid = false;
+                } else {
+                    $email = test_inputs($_POST["email"]);
+                }
+
+                if(empty($_POST["tlf"])) {
+                    $tlfErr = "Je moet je telefoonnummer invullen.";
+                    $valid = false;
+                } else {
+                    $tlf = test_inputs($_POST["tlf"]);
+                }
+                
+                if(empty($_POST["pref"])) {
+                    $prefErr = "Je moet een voorkeur kiezen";
+                    $valid = false;
+                } else {
+                    $pref = test_inputs($_POST["pref"]);
+                }
+
+                $text = test_inputs($_POST["Text1"]);
+                
+                if($valid == true){
+                    if($gender == "male"){
+                        $pronoun = "Dhr";
+                    } elseif($gender == "female") {
+                        $pronoun = "Mvr";
+                    }
+                    echo(
+                        "
+                            Dankjewel $pronoun $name! <br> <br>
+
+                            Jouw e-mail adres is $email. <br>
+                            Jouw telefoonnummer is $tlf. <br>
+                            Jouw voorkeur is $pref. <br>
+                            
+                            $text
+                        "
+                    );
+                } else {
+                    
+                    if($gender == "male") {
+                        $dropdown = '<select name="gender" id="gender">
+                            <option value="male" selected>Dhr</option>
+                            <option value="female">Mvr</option>
+                            <option value="other">Anders</option>
+                        </select><br><br>';
+                    } elseif($gender == "female") {
+                        $dropdown = '<select name="gender" id="gender">
+                            <option value="male">Dhr</option>
+                            <option value="female" selected>Mvr</option>
+                            <option value="other">Anders</option>
+                        </select><br><br>';
+                    } else {
+                        $dropdown = '<select name="gender" id="gender">
+                        <option value="male">Dhr</option>
+                        <option value="female">Mvr</option>
+                        <option value="other" selected>Anders</option>
+                    </select><br><br>';}
+
+                    if($pref == "tlf") {
+                        $radio = '<p>Communicatievoorkeur: <h3 class="error">'. $prefErr .'</h3></p>
+                            
+                            
+                        <input type="radio" id="vtlf" name="pref" value="tlf" checked>
+                        <label for="vtlf">Telefoon</label><br>
+                        
+                        <input type="radio" id="vemail" name="pref" value="email">
+                        <label for="vemail">E-mail</label><br><br>';
+                    } else {
+                        $radio = '<p>Communicatievoorkeur: <h3 class="error">'. $prefErr .'</h3></p> 
+                            
+                            
+                        <input type="radio" id="vtlf" name="pref" value="tlf" >
+                        <label for="vtlf">Telefoon</label><br>
+                        
+                        <input type="radio" id="vemail" name="pref" value="email" checked>
+                        <label for="vemail">E-mail</label><br><br>';
+                    }
+
+                }
+                    echo(
+                        '<form class="body" method="post" action="contact.php">
+                            <label for="gender">Aanhef:</label>
+    
+                            ' . $dropdown . '
+    
+                            <label for="name">Naam:</label>
+                            <input class="input" type="text" id="name" name="name" value=' . $name .'> <h3 class="error">'. $nameErr .'</h3><br><br>
+    
+                            <label for="email">E-mail adres:</label>
+                            <input class="input" type="text" id="email" name="email" value='. $email . '><h3 class="error">'. $emailErr .'</h3><br><br>
+    
+                            <label for="tlf">Telefoonnummer:</label>
+                            <input class="input" type="number" id="tlf" name="tlf"value='. $tlf .'><h3 class="error">'. $tlfErr .'</h3><br><br>
+                            
+                            ' . $radio . ' 
+    
+                            <textarea class="input" name="Text1" cols="40" rows="10">' . $text . '</textarea>
+    
+                            <br><br>
+                            <button>Submit</button>
+                        </form>'
+                    );
+                }
+                
+                
+            
 
             function test_inputs($data) {
                 $data = trim($data);
