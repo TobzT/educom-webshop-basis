889b0d288798aa96ca4480a1098d489ff9b7f15b
TobzT <tobias@conceptuals.nl>
2022-09-13 12:34:40 +0200

    php templates #7

---
+++ footer.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+
+<head>
+    <link rel="stylesheet" href="css.css">
+</head>
+
+<body>
+    <div class="container">
+        <footer>
+            &#169;
+            <p><?php echo date("Y") ?></p>
+            <p>Tobias The</p>
+        </footer>
+    </div>
+</body>
---
+++ header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<head>
+    <link rel="stylesheet" href="css.css">
+</head>
+<body>
+    <div class="container">
+        <header>
+            <h1 class="header">Title</h1>
+            <ul class="list">
+                <div class="divh"><li class="menu"><a href="home.html" class="menu">HOME</a></li></div>
+                <div class="divh"><li class="menu"><a href="about.html"class="menu">ABOUT</a></li></div>
+                <div class="divh"><li class="menu"><a href="contact.php"class="menu">CONTACT</a></li></div>
+            </ul>
+        </header>
+    </div>
+</body>
