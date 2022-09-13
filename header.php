5d75cc8bf1e83f61802aacc4bbd1fb21e42770db
TobzT <tobias@conceptuals.nl>
2022-09-13 13:57:39 +0200

    setup for new routing #7

--- header.php
+++ header.php
@@ -7,8 +7,8 @@
         <header>
             <h1 class="header">Title</h1>
             <ul class="list">
-                <div class="divh"><li class="menu"><a href="home.html" class="menu">HOME</a></li></div>
-                <div class="divh"><li class="menu"><a href="about.html"class="menu">ABOUT</a></li></div>
+                <div class="divh"><li class="menu"><a href="home.php" class="menu">HOME</a></li></div>
+                <div class="divh"><li class="menu"><a href="about.php"class="menu">ABOUT</a></li></div>
                 <div class="divh"><li class="menu"><a href="contact.php"class="menu">CONTACT</a></li></div>
             </ul>
         </header>
---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<?php 
+// include 'home.php';
+// include 'about.php';
+// include 'contact.php';
+// include 'header.php';
+// include 'footer.php';
+
+?>
+<head>
+    <link rel="stylesheet" href="css.css">
+</head>
+
+<body>
+    test test test
+</body>
+
