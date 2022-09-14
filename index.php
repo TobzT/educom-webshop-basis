1c0e30c4772b40af74a7db4ce4458c8e5c23c4d3
TobzT <tobias@conceptuals.nl>
2022-09-14 12:09:53 +0200

    routing #7

--- index.php
+++ index.php
@@ -49,11 +49,11 @@ function showHead() {
 }
 
 function showBody($page) {
-    echo('<body>');
+    echo('<body> <div class="container">');
     showHeader($page);
     showContent($page);
     showFooter();
-    echo('</body>');
+    echo('</div> </body>');
 
 }
 
@@ -89,16 +89,162 @@ function showContent($page) {
 }
 
 function showHomeContent() {
-    echo('HOME CONTENT');
+    echo('
+        <p class="body">
+            Welcome to the website, dear traveler. <br>
+            Here we will have some fun while also learning a thing or two. <br>
+            See you around traveler!
+        </p>'
+        );
 }
 
 function showAboutContent() {
-    echo('ABOUT CONTENT');
+    echo('
+        <div class="body">
+            <p>My name is Tobias, and I am building this website right now. <br>
+                I am planning on making this a bigger project, but everyone has to start somewhere.
+            </p>
+
+
+            <p>
+                My hobbies consist of:<br>
+                <ul>
+                    <li>Music production</li>
+                    <li>Video games (mainly first person shooters)</li>
+                    <li>Sports (tennis, table tennis, basket ball)</li>
+                </ul>
+            </p>
+
+            <p>If you have any questions, go over to the contact page. You can find information there.</p>
+        </div>
+    ');
 }
 
 function showContactContent() {
-    echo('CONTACT CONTENT');
+    
+    $nameErr = $emailErr = $tlfErr = $prefErr = "";
+    $name = $email = $gender = $text = $pref = $tlf = "";
+    $valid = false;
+    $pronoun = "";
+    validateContactForm();
+    
+
+    if($valid == true) {
+        switch($gender) {
+            case "male":
+                $pronoun = "dhr";
+                break;
+            case "female":
+                $pronoun = "mvr";
+        }
+        
+        switch($pref) {
+            case "email":
+                $pref = "e-mail";
+                break;
+            case "tlf":
+                $pref = "telefoon";
+        }
+        
+        showContactThanks($pronoun, $name, $email, $tlf, $pref, $text);
+    } else {
+        
+        echo(
+            '<form class="body" method="post" action="contact.php">
+                <label for="gender">Aanhef:</label>
+
+                <select name="gender" id="gender">
+                    <option value="male" ' . (($gender == "male") ? "selected" : "") . ' >Dhr</option>
+                    <option value="female" ' . (($gender == "female") ? "selected" : "") . ' >Mvr</option>
+                    <option value="other" ' . (($gender == "other") ? "selected" : "") . ' >Anders</option>
+                </select><br><br>
+
+                <label for="name">Naam:</label>
+                <input class="input" type="text" id="name" name="name" value=' . $name .'> <h3 class="error"> '. $nameErr .'</h3><br><br>
+
+                <label for="email">E-mail adres:</label>
+                <input class="input" type="text" id="email" name="email" value='. $email . '><h3 class="error"> '. $emailErr .'</h3><br><br>
+
+                <label for="tlf">Telefoonnummer:</label>
+                <input class="input" type="number" id="tlf" name="tlf"value='. $tlf .'><h3 class="error"> '. $tlfErr .'</h3><br><br>
+                
+                <p>Communicatievoorkeur: <h3 class="error"> '. $prefErr .'</h3></p>
+                
+                
+                <input type="radio" id="vtlf" name="pref" value="tlf" ' . (($pref == "tlf") ? "checked" : "") . '>
+                <label for="vtlf">Telefoon</label><br>
+                
+                <input type="radio" id="vemail" name="pref" value="email" ' . (($pref == "email") ? "checked" : "") . '>
+                <label for="vemail">E-mail</label><br><br>
+
+                <textarea class="input" name="Text1" cols="40" rows="10">' . $text . '</textarea>
+
+                <br><br>
+                <button>Submit</button>
+            </form>'
+        );
+    }
+
+    function test_inputs($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+        }
 }
+
+function validateContactForm() {
+    if($_SERVER["REQUEST_METHOD"] == "POST") {
+        $valid = true;
+        $gender = test_inputs($_POST["gender"]);
+        
+        if(empty($_POST["name"])) {
+            $nameErr = "Je moet je naam invullen.";
+            $valid = false;
+        } else {
+            $name = test_inputs($_POST["name"]);
+        }
+
+        if(empty($_POST["email"])) {
+            $emailErr = "Je moet je e-mail adres invullen.";
+            $valid = false;
+        } else {
+            $email = test_inputs($_POST["email"]);
+        }
+
+        if(empty($_POST["tlf"])) {
+            $tlfErr = "Je moet je telefoonnummer invullen.";
+            $valid = false;
+        } else {
+            $tlf = test_inputs($_POST["tlf"]);
+        }
+        
+        if(empty($_POST["pref"])) {
+            $prefErr = "Je moet een voorkeur kiezen";
+            $valid = false;
+        } else {
+            $pref = test_inputs($_POST["pref"]);
+        }
+
+        $text = test_inputs($_POST["Text1"]);
+    }
+}
+
+function showContactThanks($pronoun, $name, $email, $tlf, $pref, $text) {
+    echo(
+        '<p class="body">
+            Dankjewel ' . $pronoun . " " . ucfirst($name) . '! <br> <br>
+
+            Jouw e-mail adres is ' . $email . '. <br>
+            Jouw telefoonnummer is ' . $tlf . '. <br>
+            Jouw voorkeur is ' . $pref . '. <br> <br>
+            
+            ' . $text . '
+        </p>
+        '
+    );
+}
+
 
 function showPageError() {
     echo('
