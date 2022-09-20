af0a0766d72cb6ecfa2898056aa15436045c4b19
TobzT <tobias@conceptuals.nl>
2022-09-20 14:54:05 +0200

    test functies #9

--- users/testusers.php
+++ users/testusers.php
@@ -1,13 +1,23 @@
 <?php 
-$file = fopen("users.txt", "r");
-echo fread($file, filesize("users.txt"));
-fclose($file);
 
-$file = fopen("users.txt", "a");
-fwrite($file, "\nTEST TST TEST");
-fclose($file);
 
-$file = fopen("users.txt", "r");
-echo fread($file, filesize("users.txt"));
-fclose($file);
+function myReadFile($filename) {
+    $file = fopen($filename, "r");
+    echo fread($file, filesize("users.txt"));
+    fclose($file);
+}
+
+function myWriteFile($filename, $message) {
+    $file = fopen($filename, "a");
+    fwrite($file, $message);
+    fclose($file);
+}
+
+function findStringInFile($filename, $string) {
+    
+}
+
+
+// myWriteFile("users.txt", "\nTEST|TEST|TEST");
+myReadFile("users.txt");
 ?>
