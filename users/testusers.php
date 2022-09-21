cb12fd1d92c38b481cbe712fe9e4ff094626f666
TobzT <tobias@conceptuals.nl>
2022-09-21 11:05:28 +0200

    findEmailInFile #9

--- users/testusers.php
+++ users/testusers.php
@@ -9,15 +9,32 @@ function myReadFile($filename) {
 
 function myWriteFile($filename, $message) {
     $file = fopen($filename, "a");
-    fwrite($file, $message);
+    fwrite($file, $message . "\n");
     fclose($file);
 }
 
-function findStringInFile($filename, $string) {
-    
+function findEmailInFile($filename, $string) {
+    $file = fopen($filename, "r");
+    $result = false;
+    fgets($file);
+    while(($line = fgets($file)) !== false) {
+        // echo($line);
+        $line = explode("|", $line);
+        // echo($line[0]."\n");
+        if($line[0] == $string) {
+            $result = true;
+            break;
+        }
+    }
+
+    fclose($file);
+    return $result;
 }
 
 
-// myWriteFile("users.txt", "\nTEST|TEST|TEST");
-myReadFile("users.txt");
+// myWriteFile("users.txt", "TEST|TEST|TEST");
+// myReadFile("users.txt");
+findEmailInFile("users.txt", "Tobias@conceptuals.nl");
+// findEmailInFile("users.txt", "TEST");
+
 ?>
