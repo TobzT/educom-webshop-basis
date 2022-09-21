289e5940a37e36706ee63e8aa1d34740a0d172de
TobzT <tobias@conceptuals.nl>
2022-09-21 11:34:01 +0200

    vooruitgang validateRegistration #9

--- register.php
+++ register.php
@@ -1,7 +1,10 @@
 <?php 
 require_once("contact.php");
 function showRegisterContent() {
-    // if()
+    if($_SERVER['REQUEST_METHOD'] == "POST") {
+
+        validateRegistration();
+    }
     
     showFormStart();
 
@@ -10,5 +13,58 @@ function showRegisterContent() {
     showFormItem("pw", "password", "Password:", "", "");
     showFormItem("cpw", "password", "Confirm Password:", "", "");
     showFormEnd("register", "Sign Up");
+}
+
+function validateRegistration($filename) {
+    $pwErr = $emailErr = "";
+    $valid = true;
+    $name = test_inputs(getVarFromArray($_POST, "name"));
+    $email = strtolower(test_inputs(getVarFromArray($_POST, "email")));
+    $pw = test_inputs(getVarFromArray($_POST, "pw"));
+    $cpw = test_inputs(getVarFromArray($_POST, "cpw"));
+
+    if ($pw !== $cpw){
+        $valid = false;
+        $pwErr = "Passwords do not match";
+    }
+
+    if (findEmailInFile("users.txt", $email)) {
+        $valid = false;
+        $emailErr = "E-mail found in database";
+    }
+
+    
+    if ($valid) {
+        myWriteToFile($filename, $message);
+    }
+    
+    return array("valid" => $valid, "pwErr" => $pwErr, "emailErr" => $emailErr);
+
+}
+
+function myWriteFile($filename, $message) {
+    $file = fopen($filename, "a");
+    fwrite($file, $message . "\n");
+    fclose($file);
+}
+
+
+// returns true if email in file
+function findEmailInFile($filename, $string) {
+    $file = fopen($filename, "r");
+    $result = false;
+    fgets($file);
+    while(($line = fgets($file)) !== false) {
+        // echo($line);
+        $line = explode("|", $line);
+        // echo($line[0]."\n");
+        if($line[0] == $string) {
+            $result = true;
+            break;
+        }
+    }
+
+    fclose($file);
+    return $result;
 }
 ?>
