403a01c40e3b053ad2cda75024aeb849758dd043
TobzT <tobias@conceptuals.nl>
2022-09-22 10:37:24 +0200

    login form added #10

--- login.php
+++ login.php
@@ -1,5 +1,9 @@
 <?php 
+require_once("contact.php");
 function ShowLoginContent(){
-    
+    showFormStart();
+    showFormItem("email", "email", "E-mail:", "", "");
+    showFormItem("pw", "password", "Password:", "", "");
+    showFormEnd("login", "Log In");
 }
 ?>
