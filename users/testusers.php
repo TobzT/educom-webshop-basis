deb8294bc0a9be88807df061baebf01d91bb6a25
TobzT <tobias@conceptuals.nl>
2022-09-27 15:48:05 +0200

    test files en db #10

--- users/testusers.php
+++ users/testusers.php
@@ -1,5 +1,75 @@
 <?php 
+function ShowFormStart() {
+    echo('<form class="body" method="post" action="index.php"s>');
+}
 
+function ShowFormEnd($page, $submitText) {
+    echo('<input type="hidden" name="page" value="'.$page.'">');
+    echo('<button>'.$submitText.'</button></form>');
+}
+
+function showMetaForm($page, $meta) {
+
+    showFormStart();
+
+    foreach($meta as $array){
+        showMetaFormItem($array);
+    }
+    showFormEnd($page, "Submit");
+}
+
+function showMetaFormItem($array) {
+    echo('<div>
+        <label for="'.$array['key'].'">'.$array['labeltext'].'</label>'
+    );
+    
+    switch ($array['type']) {
+        case "dropdown":
+            echo('
+                    <select name="'.$array['key'].'" id="'.$array['key'].'" >');
+
+            echo(repeatingForm($array['options'], $array['value']));
+
+            echo('</select>');
+            break;
+        
+        case "radio":
+            echo('
+                <p><h3 class="error"> '. $array['error'] .'</h3></p>
+            ');
+
+            echo(repeatingRadio($array['key'], $array['options']));
+
+            break;
+        
+        case "textarea":
+            echo('
+                
+                <textarea class=input name="'.$array['key'].'" cols="40" rows="10"></textarea>
+
+                
+            ');
+            break;
+        
+        default:
+            echo('
+                    <input class="input" type="'.$array['type'].'" id="'.$array['key'].'" name="'.$array['key'].'" value="'. $array['type'] .'">
+                    
+                    <h3 class="error">'.$array['error'].'</h3>
+                
+            ');
+            break;
+    }
+    echo('</div><br>');
+}
+
+
+$test = array(
+    array('name' => 'lmao', 'type' => 'text', 'key' => 'lmao', 'labeltext' => 'help', 'error' => ''),
+    array('name' => 'hoppa', 'type' => 'textarea', 'key' => 'hoppa', 'labeltext' => 'bingus', 'error' => '')
+);
+
+// showMetaForm('nvm', $test);
 
 function myReadFile($filename) {
     $file = fopen($filename, "r");
@@ -45,8 +115,8 @@ function findEmailInFile($filename, $string) {
     fclose($file);
     return $result;
 }
-$data = findEmailInFile("users.txt", "balls@mogus.nl");
-var_dump($data);
+// $data = findEmailInFile("users.txt", "balls@mogus.nl");
+// var_dump($data);
 
 function session_test(){
     session_start();
@@ -61,6 +131,7 @@ function session_test(){
 // findEmailInFileB("users.txt", "Tobias@conceptuals.nl");
 // findEmailInFileB("users.txt", "TEST");
 // session_test();
-findEmailInFile("users.txt", "niks@niemand.nl");
+// findEmailInFile("users.txt", "niks@niemand.nl");
 
+phpinfo();
 ?>
